28f0484df4bfa566ed0c4c156347e31b75c5b7aa
Valentine Omonya <valomosh254@gmail.com>
2024-05-23 11:34:46 +0300

    Added hide/Show navbar functionality

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import MaxWidthContainer from "../MaxWidthContainer/MaxWidthContainer";
 const navbarLinks = [
   {
@@ -27,6 +27,7 @@ const navbarLinks = [
   },
 ];
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <MaxWidthContainer>
       <header className="w-full h-[4rem] flex justify-between items-center">
@@ -34,7 +35,7 @@ const Navbar = () => {
           <span className="text-purple-700">De</span>v
           <span className="text-purple-500">Br</span>os
         </div>
-        <nav className={`fixed md:static  w-1/2 md:w-full bg-sky-50 top-[4rem] bottom-0 right-0`}>
+        <nav className={`fixed md:static  w-1/2 md:w-full bg-slate-50 top-[4rem] bottom-0  transition-all ease-in-out duration-300  ${isOpen ? "right-0" : "-right-1/2"}`}>
           <ul className="flex items-start md:justify-center pl-4 md:pl-0 flex-col md:flex-row gap-x-6 ">
             {navbarLinks &&
               navbarLinks.map((navLink, index) => (
@@ -51,7 +52,7 @@ const Navbar = () => {
           <button className="rounded-full border px-4 py-2 border-blue-400 text-blue-400 font-semibold">
             Login/Signup
           </button>
-          <div>
+          <div onClick={()=>setIsOpen(prev=>!prev)}>
             menu
           </div>
         </div>
