317f50c76fcb8d3b44ce2190de37740c5aabc2d1
Valentine Omonya <valomosh254@gmail.com>
2024-05-24 22:46:40 +0300

    Added custome scroll items

--- src/components/EventsCard/EventsCard.jsx
+++ src/components/EventsCard/EventsCard.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import testImage from "../../assets/pattern1.png";
-const EventsCard = () => {
+const EventsCard = ({eventItem}) => {
+
   return (
     <div className="w-full aspect-square">
       <div className="h-1/2 relative">
-        <img src={testImage} className="absolute w-full h-full rounded-xl " />
+        <img src={eventItem.image} className="absolute w-full h-full rounded-xl " />
       </div>
       <div className="mt-2">
-        <p className="text-purple-500 font-[400]">
-          <span>WED, MAR 15</span> | <span>7:30 PM BST</span>{" "}
+        <p className="text-[#9380f6] font-[400]">
+          <span>{eventItem.date}</span> | <span>{eventItem.time}</span>
         </p>
         <h2 className="text-slate-900 font-semibold text-3xl ">
-          Carrer meetup with Design & Development
+          {eventItem.title}
         </h2>
       </div>
       <div className="flex justify-between items-center mt-4">
@@ -21,7 +21,7 @@ const EventsCard = () => {
           <div className="w-10 h-10 rounded-full bg-yellow-500 -ml-5 hover:ml-0 hover:scale-105 transition-all ease-in-out duration-300 flex justify-center items-center "></div>
           <div className="w-10 h-10 rounded-full bg-purple-500 -ml-5 hover:ml-0 hover:scale-105 transition-all ease-in-out duration-300 flex justify-center items-center "></div>
           <div className="w-10 h-10 rounded-full bg-orange-500 -ml-5 hover:ml-0 hover:scale-105 transition-all ease-in-out duration-300 flex justify-center items-center ">
-            50+
+          {eventItem.attendees}+
           </div>
         </div>
         <h4 className="text-slate-800 font-semibold text-xl">View more</h4>
@@ -29,5 +29,6 @@ const EventsCard = () => {
     </div>
   );
 };
+
 
 export default EventsCard;
--- src/components/EventsSection/EventsSection.jsx
+++ src/components/EventsSection/EventsSection.jsx
@@ -2,21 +2,127 @@ import React from "react";
 import EventsCard from "../EventsCard/EventsCard";
 import { FaArrowLeft } from "react-icons/fa6";
 import { FaArrowRight } from "react-icons/fa";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import ai_talk from "../../assets/ai_talk.jpg";
+import career_meetup from "../../assets/career_meetup.jpg";
+import digital_marketing from "../../assets/digital_marketing.jpeg";
+import startup_competition from "../../assets/startup_competition.jpg";
+import tech_exhibition from "../../assets/tech_exhibition.jpg";
+import web_development_workshop from "../../assets/web_development_workshop.jpg";
+const events = [
+  {
+    date: "WED, MAR 15",
+    time: "7:30 PM BST",
+    title: "Career meetup with Design & Development",
+    attendees: 50,
+    image: career_meetup,
+  },
+  {
+    date: "THU, APR 20",
+    time: "6:00 PM EST",
+    title: "Digital Marketing Conference",
+    attendees: 120,
+    image: digital_marketing,
+  },
+  {
+    date: "FRI, JUN 25",
+    time: "8:00 AM PST",
+    title: "Startup Pitch Competition",
+    attendees: 80,
+    image: startup_competition,
+  },
+  {
+    date: "SAT, JUL 10",
+    time: "2:00 PM GMT",
+    title: "Tech Exhibition Opening",
+    attendees: 30,
+    image: tech_exhibition,
+  },
+  {
+    date: "MON, AUG 30",
+    time: "5:30 PM CET",
+    title: "Tech Talk: AI and Future of Work",
+    attendees: 90,
+    image: ai_talk,
+  },
+  {
+    date: "WED, SEP 5",
+    time: "9:00 AM BST",
+    title: "Web Development Workshop",
+    attendees: 60,
+    image: web_development_workshop,
+  },
+];
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3,
+    slidesToSlide: 3,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+    slidesToSlide: 2,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+    slidesToSlide: 1,
+  },
+};
 
 const EventsSection = () => {
+  const CustomLeftArrow =({ onClick }) => {
+ 
+    return (
+      <div onClick={() => onClick()} className="cursor-pointer">
+        <FaArrowLeft size={24} />
+      </div>
+    );
+  };
+
+  const CustomRightArrow =({ onClick }) => {
+ 
+    return (
+      <div onClick={() => onClick()} className="cursor-pointer">
+        <FaArrowRight size={24} />
+      </div>
+    );
+  };
   return (
     <div className="mb-6">
       <div className="flex justify-between items-center mb-6 mt-8">
         <h3 className="text-4xl text-slate-900 font-semibold">
           Upcoming Online Events
         </h3>
-        <div className="flex items-center gap-x-2"><FaArrowLeft size={24}className="cursor-pointer" /> <FaArrowRight size={24} className="cursor-pointer" /></div>
+        <div className="flex items-center gap-x-2">
+          <CustomLeftArrow />
+          <CustomRightArrow />
+        </div>
       </div>
-      <div className=" grid grid-cols-3 w-full gap-x-6">
-        <EventsCard />
-        <EventsCard />
-        <EventsCard />
-      </div>
+
+      <Carousel
+        customLeftArrow={<CustomLeftArrow />}
+        customRightArrow={<CustomRightArrow />}
+        swipeable={true}
+        draggable={false}
+        showDots={true}
+        responsive={responsive}
+        ssr={true}
+        infinite={true}
+        keyBoardControl={true}
+        customTransition="all .5"
+        transitionDuration={500}
+        containerClass="carousel-container"
+        removeArrowOnDeviceType={["tablet", "mobile"]}
+        itemClass="carousel-item-padding-40-px"
+      >
+        {events &&
+          events.map((eventItem, i) => (
+            <EventsCard key={i} eventItem={eventItem} />
+          ))}
+      </Carousel>
     </div>
   );
 };
