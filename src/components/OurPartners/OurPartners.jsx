b5b929d2ac95de806d9187e932c54d89bdfd56a5
Valentine Omonya <valomosh254@gmail.com>
2024-05-24 21:10:22 +0300

    Added Partners section

--- src/components/OurPartners/OurPartners.jsx
+++ src/components/OurPartners/OurPartners.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-
+import { CiSquarePlus } from "react-icons/ci";
+import { FaAmazon, FaGoogle, FaSlack, FaYoutube } from "react-icons/fa6";
 const OurPartners = () => {
   return (
     <div className="w-full h-[60vh] bg-gray-200  rounded-xl flex items-center flex-col py-24">
@@ -9,9 +10,32 @@ const OurPartners = () => {
       <h1 className="text-5xl font-semibold text-slate-900 mt-4 ">
         Together to Inspire Freashiers
       </h1>
-      <p>
-        
+      <p className="mt-10 text-base text-center px-12  lg:w-[55%]">
+        Ullamco consectetur esse tempor fugiat do ut sunt ex labore. Ullamco
+        adipisicing quis duis magna reprehenderit do qui cupidatat. Ullamco
+        adipisicing eiusmod ex nulla laboris cupidatat. Duis consequat cillum
       </p>
+      <button className=" mt-4 flex items-center py-3 px-6 rounded-full gap-x-3 font-semibold text-gray-50  bg-yellow-400">
+        <CiSquarePlus size={20} /> More About Us
+      </button>
+      <div className="flex items-center gap-x-4 mt-8">
+        <div className="flex items-center gap-x-2">
+          <FaGoogle size={40}/>
+          <span className="text-2xl font-semibold">Google</span>
+        </div>
+        <div className="flex items-center gap-x-2">
+          <FaAmazon size={40}/>
+          <span className="text-2xl font-semibold">Amazon</span>
+        </div>
+        <div className="flex items-center gap-x-2">
+          <FaYoutube size={40}/>
+          <span className="text-2xl font-semibold">Youtube</span>
+        </div>
+        <div className="flex items-center gap-x-2">
+          <FaSlack size={40}/>
+          <span className="text-2xl font-semibold">Slack</span>
+        </div>
+      </div>
     </div>
   );
 };
